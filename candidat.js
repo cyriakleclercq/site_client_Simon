3a0aaf459820c71a37aff65e06a52d512c9529b2
la.richet@laposte.net <Trocme11>
2018-10-24 16:13:30 +0200

    version.steven

---
+++ candidat.js
@@ -0,0 +1,10 @@
+
+
+function vaEtVient(){
+    if(document.getElementById("slogan").style.display == 'block'){
+        document.getElementById("slogan").style.display = 'none';
+    }
+    else {
+        document.getElementById("slogan").style.display = 'none';
+    }
+}
